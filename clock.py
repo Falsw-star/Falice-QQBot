de1b5f85a757b5bc4264056ad59077140fe18335
Falsw <Falsestar114514@outlook.com>
2024-05-28 23:46:49 +0800

    在clock中实现get用户回复
    # WARN：未经测试的功能
    ssend, get两个函数功能未经过测试，请谨慎使用
    *修复了一点bug*

--- clock.py
+++ clock.py
@@ -32,9 +32,31 @@ make_log = False
 
 #预定消息发送列表
 SCHEDULEDSEND = []
+
+#获取用户回复列表和回复列表
+GOTLIST = []
+GOT_RSP = []
+
 def ssend(channel_id, content):
     global SCHEDULEDSEND
     SCHEDULEDSEND.append([channel_id, content])
+
+def get(channel_id, user_id, message_id, ask_content, timeout: int = 20):
+    global GOTLIST
+    GOTLIST.append([channel_id, user_id, message_id])
+    message_create(channel_id, f"<at is='{user_id}'> " + ask_content)
+    for i in range(0, timeout):
+        if GOT_RSP:
+            for rsp in GOT_RSP:
+                if rsp[0] == channel_id and rsp[1] == user_id and rsp[2] == message_id:
+                    rsp_content = rsp[3]
+                    GOT_RSP.remove(rsp)
+                    return rsp_content
+        time.sleep(1)
+    #超时,取消任务
+    GOTLIST.remove([channel_id, user_id, message_id])
+    message_create(channel_id, f"等待{user_id}回复超时({timeout}s)")
+    return None
 
 #主时钟
 log("启动主时钟...","DEBUG")
@@ -44,13 +66,27 @@ while True:
     elif adapter.STATUS == True:
         #运行被注册为“service”的插件
         run_services()
+
+        #消息处理
         for msg in adapter.MASSAGE_LIST:
+
             if make_log == True:
                 thread.start_new_thread(save,(msg,))
             thread.start_new_thread(match,(msg,))
+
+            if GOTLIST:
+                msg_channel_id = msg["guild"]["id"]
+                msg_user_id = msg["user"]["id"]
+                for got in GOTLIST:
+                    if got[0] == msg_channel_id and got[1] == msg_user_id:
+                        GOT_RSP.append([msg_channel_id, msg_user_id, got[2], msg["content"]])
+                        GOTLIST.remove(got)
+
+            adapter.MASSAGE_LIST.remove(msg)
+
+        #预定发送消息
         if SCHEDULEDSEND:
             thread.start_new_thread(message_create,(SCHEDULEDSEND[0][0], SCHEDULEDSEND[0][1]))
             SCHEDULEDSEND.remove(SCHEDULEDSEND[0])
 
-            adapter.MASSAGE_LIST.remove(msg)
     time.sleep(1)
